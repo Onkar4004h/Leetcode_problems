1d0b646a18fffc149c6c28606a7373f1f1bef063
Onkar4004h <onkarraut633@gmail.com>
2026-08-09 16:07:17 +0530

    2nd try to optimize

--- 3_sumAS2sum.py
+++ 3_sumAS2sum.py
@@ -1,15 +1,17 @@
-nums = [0,0,0,0]
+nums = [-1,0,1,2,-1,-4]
 nums.sort()
 l = 0
 r = len(nums) - 1
 x=len(nums)-1
 ans_array =[]
+
 while x!=0:
+
   target=(-nums[x])
   if x!=l and l!=r and x!=r:
     if nums[l]+nums[r]==target:
         y = [nums[l],nums[r],nums[x]]
-        y.sort()
+        
         if y not in ans_array:
            ans_array.append(y)
            l+=1
@@ -26,7 +28,7 @@ while x!=0:
   if r <= l:
         x -= 1
         l = 0
-        r = len(nums) - 1 
+        r = x - 1 
  
 
 
